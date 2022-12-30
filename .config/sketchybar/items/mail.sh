e3953118669dd1226b86039788bbf05d570979cf
ericlovesmath <ericlovesmath@gmail.com>
2022-12-30 01:14:03 -0800

    Sketchybar Spotify Update

--- .config/sketchybar/items/mail.sh
+++ .config/sketchybar/items/mail.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-sketchybar --add item mail right                   \
+sketchybar --add item mail left                   \
            --set mail update_freq=120              \
                       script="$PLUGIN_DIR/mail.sh" \
                       background.padding_left=5  \
