fa4737decd622129277f2ef3740e089cf5ebf955
ericlovesmath <ericlovesmath@gmail.com>
2023-03-15 21:49:20 -0700

    Add ability to remove notifications from bar

--- .config/sketchybar/items/mail.sh
+++ .config/sketchybar/items/mail.sh
@@ -3,6 +3,7 @@
 sketchybar --add item mail left                   \
            --set mail update_freq=120              \
                       script="$PLUGIN_DIR/mail.sh" \
+                      click_script="$PLUGIN_DIR/mail.sh" \
                       icon.padding_left=22               \
                       icon.font="$ICON_FONT:Solid:16.0"  \
                       icon=$MAIL                   \
--- .config/sketchybar/items/messages.sh
+++ .config/sketchybar/items/messages.sh
@@ -3,6 +3,7 @@
 sketchybar --add item messages left                   \
            --set messages update_freq=120              \
                       script="$PLUGIN_DIR/messages.sh" \
+                      click_script="$PLUGIN_DIR/messages.sh" \
                       icon.padding_left=22               \
                       background.padding_right=-15               \
                       icon.font="$ICON_FONT:Solid:16.0"  \
