e1994e5d399633b51a61618a953f366af19e6831
ericlovesmath <ericlovesmath@gmail.com>
2024-04-22 16:32:42 -0700

    Scratchpad for yabai (Spotify) disabled, minor updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,11 +4,11 @@
     Github: https://github.com/ericlovesmath
 --]]
 
+vim.loader.enable()
+
 require("config.options")
 require("config.mappings")
 require("config.lazy")
 require("config.status")
 require("config.runner")
 require("config.utils")
-
-vim.loader.enable()
--- bin/cheat.sh
+++ bin/cheat.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-LANGUAGES=$(echo "golang lua cpp typescript python java csharp rust css" | tr ' ' '\n')
+LANGUAGES=$(echo "golang lua cpp typescript python java csharp rust css haskell ocaml bash" | tr ' ' '\n')
 CORE_UTILS=$(echo "xargs find mv sed awk" | tr ' ' '\n')
 
 SELECTED=$(printf "$LANGUAGES\n$CORE_UTILS" | fzf)
 echo $SELECTED
+
 read -p "Query: " QUERY
+QUERY=$(printf %s "$QUERY" | jq -sRr @uri)
 
 #if printf $LANGUAGES | grep -qs $SELECTED; then
 if grep -q "$SELECTED" <<< "$LANGUAGES"; then
-    curl cht.sh/$SELECTED/$(echo $QUERY | tr ' ' '+')
+    curl cht.sh/$SELECTED/$QUERY
 else
-    curl cht.sh/$SELECTED~$(echo $QUERY | tr ' ' '+')
+    curl cht.sh/$SELECTED~$QUERY
 fi
-
