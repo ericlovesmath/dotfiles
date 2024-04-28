73ef594f0fcd869e377096329bb12acec192f1e9
ericlovesmath <ericlovesmath@gmail.com>
2024-04-28 11:24:48 -0700

    Fix Ultisnips and Tabbing for React files

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -34,7 +34,7 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 augroup("TwoTabWidth", { clear = true })
-for _, ft in pairs({ "c", "css", "html", "javascript", "typescript", "ocaml" }) do
+for _, ft in pairs({ "c", "css", "html", "javascript", "typescript", "ocaml", "javascriptreact", "typescripreact" }) do
     autocmd("Filetype", {
         group = "TwoTabWidth",
         pattern = ft,
