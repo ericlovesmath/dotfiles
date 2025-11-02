cbc53e60198ff0baeb5529496464eb47e9ea32d9
Eric Lee <me@ericchanlee.com>
2025-11-02 15:44:39 -0800

    Persistent spotify icon in status bar if Spotify is on

--- waybar/music.sh
+++ waybar/music.sh
@@ -5,5 +5,8 @@ PLAYER_STATUS=$(playerctl -p spotify status 2> /dev/null)
 if [ "$PLAYER_STATUS" = "Playing" ]; then
     ARTIST=$(playerctl -p spotify metadata artist | sed 's/&/&amp;/g')
     TITLE=$(playerctl -p spotify metadata title | sed 's/&/&amp;/g')
-    echo "$ARTIST - $TITLE"
+    echo " $ARTIST - $TITLE"
+fi
+if [ "$PLAYER_STATUS" = "Paused" ]; then
+    echo ""
 fi
